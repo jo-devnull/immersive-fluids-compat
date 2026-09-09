2780563fc917c80a91a6e4037ee2c28db8d17e2b
jo-devnull <jorge.devnull@gmail.com>
2026-09-09 02:14:36 -0300

    fix mixin crash

--- src/main/java/github/jodevnull/ifluidscompat/mixin/MixinCachedWater.java
+++ src/main/java/github/jodevnull/ifluidscompat/mixin/MixinCachedWater.java
@@ -2,15 +2,17 @@ package github.jodevnull.ifluidscompat.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import github.jodevnull.ifluidscompat.IFluidsCompat;
+import io.github.SirWashington.WaterPhysics;
 import io.github.SirWashington.features.CachedWater;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,21 +21,53 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static github.jodevnull.ifluidscompat.IFluidsCompat.WORLDGEN;
 
 @Mixin(value = CachedWater.class, remap = false)
-public class MixinCachedWater
+public abstract class MixinCachedWater
 {
+    @Shadow
+    public static BlockState getBlockState(BlockPos pos) {
+        throw new UnsupportedOperationException("Implemented via mixin");
+    }
+
+    @Shadow
+    public static void setBlockStateNoNeighbors(BlockPos pos, BlockState oldState, BlockState state) {
+        throw new UnsupportedOperationException("Implemented via mixin");
+    }
+
+    @Shadow
+    public static Level world;
+
     @Unique
     private static boolean ifc$isNaturalSource(BlockPos pos) {
         return IFluidsCompat.isWorldgen(CachedWater.getBlockState(pos));
     }
 
-    @WrapOperation(method = "setWaterLevelDirect", at= @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;defaultBlockState()Lnet/minecraft/world/level/block/state/BlockState;", ordinal = 1))
-    private static BlockState ifc$setWaterLevelDirect(Block instance, Operation<BlockState> original) {
-        return Blocks.WATER.defaultBlockState().setValue(WORLDGEN, false);
-    }
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    private static void setWaterLevelDirect(int level, BlockPos pos) {
+        BlockState prev = getBlockState(pos);
 
-    @WrapOperation(method = "setWaterLevelDirect", at= @At(value = "INVOKE", target = "Lnet/minecraft/world/level/material/FluidState;createLegacyBlock()Lnet/minecraft/world/level/block/state/BlockState;"))
-    private static BlockState ifc$setWaterLevelDirect2(FluidState instance, Operation<BlockState> original) {
-        return original.call(instance).setValue(WORLDGEN, false);
+        assert prev.isAir() || prev.hasProperty(WaterPhysics.WATER_LEVEL) || !prev.getFluidState().isEmpty() || level < 0;
+
+        if (prev.hasProperty(WaterPhysics.WATER_LEVEL)) {
+            setBlockStateNoNeighbors(pos, prev, prev.setValue(WaterPhysics.WATER_LEVEL, level));
+        } else if (level == 0) {
+            setBlockStateNoNeighbors(pos, prev, Blocks.AIR.defaultBlockState());
+        } else if (level >= 0) {
+            if (level <= 8) {
+                if (level == 8) {
+                    if (!(prev.getBlock() instanceof LiquidBlockContainer))
+                        setBlockStateNoNeighbors(pos, prev, Blocks.WATER.defaultBlockState().setValue(WORLDGEN, false));
+                } else {
+                    if (!(prev.getBlock() instanceof BucketPickup))
+                        world.destroyBlock(pos, true);
+
+                    setBlockStateNoNeighbors(pos, prev, Fluids.FLOWING_WATER.getFlowing(level, false).createLegacyBlock().setValue(WORLDGEN, false));
+                }
+            }
+        }
     }
 
     @WrapMethod(method = "isInfinite")
